65ce0f8a7bb8825830e2342f4617e72bcbd4b80b
Christina <christina.gaitan@gmail.com>
2020-12-14 08:57:35 -0600

    Use NavLinks instead of Links, exact and active css class

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
@@ -11,13 +11,21 @@ class Blog extends Component {
               <header>
                 <nav>
                   <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to={{
+                    {/* // To set a custom active class
+                    <li><NavLink activeClassName='my-active' to='/' exact>Home</NavLink></li> */}
+
+                    {/* // To set active inline style
+                    <li><NavLink activeStyle={{
+                      color: 'red',
+                      textDecoration: 'underline' }} to='/' exact>Home</NavLink></li> */}
+
+                    <li><NavLink to='/' exact>Home</NavLink></li>
+                    <li><NavLink to={{
                         // pathname: this.props.match.url + '/new-post', // Use this to build a relative path
                         pathname: '/new-post', // This is an absolute path
                         hash: '#submit',
                         search: '?quick-submit=true'
-                      }}>New Post</Link></li>
+                      }}>New Post</NavLink></li>
                   </ul>
                 </nav>
               </header>
